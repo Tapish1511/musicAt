20629c6fd474ef98974b9f4fedcc34c7fb18c7fb
Tapish1511 <87308840+Tapish1511@users.noreply.github.com>
2023-01-01 23:48:23 +0530

    resized the main body height

--- Scripts/style.js
+++ Scripts/style.js
@@ -2,12 +2,13 @@ let bodyEl = document.querySelector('body');
 let headEl = document.querySelector('.head-container');
 let mainBody = document.getElementById('main-body');
 const sidePanel = document.querySelector('.side-pannel');
-
+let player = document.getElementById('player');
 
 let navHeight = headEl.getBoundingClientRect().height;
+let playerHeight = player.getBoundingClientRect().height
 
 bodyEl.style.gridTemplateRows = `${navHeight}px auto`
-mainBody.style.height = `calc(100vh - ${navHeight}px)`
+mainBody.style.height = `calc(100vh - ${navHeight}px - ${playerHeight}px)`
 // mainBody.style.transform = `translateY(${navHeight}px)`
 
 
